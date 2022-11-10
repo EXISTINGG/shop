ac5c3603341b9835832912c246b38c78fce95cc2
EXISTINGG <1906054192@qq.com>
2022-11-10 13:50:35 +0800

    完成了 home 首页的开发

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,47 @@
 <template>
 	<view>
-		Home
+		<!-- 轮播图区域 -->
+		<swiper autoplay :interval="3000" :duration="1000" :circular="true" indicator-dots
+			indicator-active-color="rgba(192, 0, 0,.7)">
+			<swiper-item v-for="(item, index) in swiperList" :key="index">
+				<navigator class="swiper-item" @click="swiperGoTo(item.goods_id)">
+					<image :src="item.image_src" mode=""></image>
+				</navigator>
+			</swiper-item>
+		</swiper>
+		<!-- 分类导航区域 -->
+		<view class="nav-list">
+			<view class="nav-item" v-for="(item, i) in navList" :key="i" @click="navClickHandler(item.name)">
+				<image :src="item.image_src" mode="" class="nav-img"></image>
+			</view>
+		</view>
+		<!-- 楼层区域 -->
+		<view class="floor-list">
+			<!-- 楼层 item 项 -->
+			<view class="floor-item" v-for="(item, i) in floorList" :key="i">
+				<!-- 楼层标题 -->
+				<image :src="item.floor_title.image_src" mode="" class="floor-title"></image>
+				<!-- 楼层图片区域 -->
+				<view class="floor-img-box">
+					<!-- 左侧大图片的盒子 -->
+					<navigator class="left-img-box" :url="item.product_list[0].url">
+						<image :src="item.product_list[0].image_src" mode="widthFix"
+							:style="{width: item.product_list[0].image_width + 'rpx'}">
+						</image>
+					</navigator>
+					<!-- 右侧 4 个小图片的盒子 -->
+					<view class="right-img-box">
+						<navigator class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2"
+							v-if="i2 !== 0" :url="item2.url">
+							<image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width + 'rpx'}">
+							</image>
+						</navigator>
+
+					</view>
+				</view>
+			</view>
+
+		</view>
 	</view>
 </template>
 
@@ -8,12 +49,120 @@
 	export default {
 		data() {
 			return {
-				
+				// 轮播图的数据列表，默认为空数组
+				swiperList: [],
+				// 分类导航的数据列表
+				navList: [],
+				// 楼层的数据列表
+				floorList: []
 			};
+		},
+		//  在小程序页面刚加载的时候，调用方法
+		onLoad() {
+			this.getSwiperList()
+			this.getNavList()
+			this.getFloorList()
+		},
+		methods: {
+			//  获取轮播图数据的方法
+			async getSwiperList() {
+				// 发起请求
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				//请求失败
+				if (res.meta.status !== 200) return uni.$showMsg()
+				//请求成功
+				this.swiperList = res.message
+			},
+			//获取分类导航数据的方法
+			async getNavList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/catitems')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.navList = res.message
+			},
+			// 获取楼层数据的方法
+			async getFloorList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/floordata')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				// 通过双层 forEach 循环，处理 URL 地址
+				res.message.forEach(floor => {
+					floor.product_list.forEach(prod => {
+						//增加一个 url 属性
+						prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[
+							1] // 表示只要后面的数据(query=服饰)
+					})
+				})
+				this.floorList = res.message
+				console.log(this.floorList);
+			},
+
+
+			//轮播图跳转编程式导航
+			swiperGoTo(id) {
+				console.log(id);
+				uni.navigateTo({
+					url: `/subpkg/goods_detail/main?goods_id=${id}`
+				})
+			},
+
+			// nav-item 项被点击时候的事件处理函数
+			navClickHandler(item) {
+				if (item === '分类') {
+					uni.switchTab({
+						url: '/pages/cate/cate'
+					})
+				}
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 330rpx;
 
+		.swiper-item,
+		image {
+			width: 100%;
+			height: 100%;
+		}
+	}
+
+	.nav-list {
+		display: flex;
+		justify-content: space-around;
+		margin: 15px 0;
+
+		.nav-img {
+			width: 128rpx;
+			height: 140rpx;
+		}
+	}
+
+	.floor-list {
+
+		.floor-item {
+			.floor-title {
+				height: 60rpx;
+				width: 100%;
+				display: flex;
+			}
+
+			.right-img-box {
+				display: flex;
+				flex-wrap: wrap;
+				justify-content: space-around;
+			}
+
+			.floor-img-box {
+				display: flex;
+				padding-left: 10rpx;
+			}
+		}
+	}
 </style>
